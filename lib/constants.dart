911767ebea1a3bd05f45e0eae79afa9adcd50993
Varunkashyap2303 <r.varunkashyap99@gmail.com>
2020-01-21 23:28:01 +0530

    Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+Color appDarkGreyColor = Color.fromRGBO(58, 66, 86, 1.0);
+Color appGreyColor = Color.fromRGBO(64, 75, 96, .9);
+
+const appTitle = "Contactly";
+const pinCodeHintText = "Pin Code";
+const loginButtonText = "Login";
+
+Image appLogo = Image.asset('assets/images/flutter-logo-round.png');
+
+const bigRadius = 66.0;
+const buttonHeight = 24.0;
+
+const loginPageTag = 'Login Page';
+const homePageTag = 'Home Page';
